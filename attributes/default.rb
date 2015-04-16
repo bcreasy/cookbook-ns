788c899816ded952f0b0d44645eb0dba6f21fdeb
Brian Creasy <genetik@gmail.com>
2015-04-16 03:01:15 -0500

    ugh.. typo - stop spamming some random person

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ node.default['local']['hostname'] = 'ns1.caffeine.nu'
 node.default['local']['ip4addr'] = '104.131.63.61'
 
 node.default['primary']['hostname'] = 'ns2.caffeine.nu'
-node.default['primary']['ip4addr'] = '208.52.148.24'
+node.default['primary']['ip4addr'] = '208.52.168.244'
