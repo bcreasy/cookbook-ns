d0eacd84107ed46768842b839e5d81105af4c0ca
Brian Creasy <genetik@gmail.com>
2015-04-16 01:37:56 -0500

    secondary dns config

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,53 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'bind9' do
+  action :install
+end
+
+template "/etc/bind/named.conf" do
+  source "named.conf"
+  owner "root"
+  group "bind"
+  mode "0644"
+end
+
+template "/etc/bind/named.conf.acl" do
+  source "named.conf.acl.erb"
+  owner "root"
+  group "bind"
+  mode "0644"
+  variables(
+    :local_hostname   => node['local']['hostname'],
+    :local_ip4addr    => node['local']['ip4addr'],
+    :primary_hostname => node['primary']['hostname'],
+    :primary_ip4addr  => node['primary']['ip4addr'],
+  )
+end
+
+template "/etc/bind/named.conf.options" do
+  source "named.conf.options.erb"
+  owner "root"
+  group "bind"
+  mode "0644"
+  variables(
+    :local_hostname   => node['local']['hostname'],
+    :local_ip4addr    => node['local']['ip4addr'],
+    :primary_hostname => node['primary']['hostname'],
+    :primary_ip4addr  => node['primary']['ip4addr'],
+  )
+end
+
+template "/etc/bind/named.conf.local" do
+  source "named.conf.local.erb"
+  owner "root"
+  group "bind"
+  mode "0644"
+  variables(
+    :local_hostname   => node['local']['hostname'],
+    :local_ip4addr    => node['local']['ip4addr'],
+    :primary_hostname => node['primary']['hostname'],
+    :primary_ip4addr  => node['primary']['ip4addr'],
+  )
+end
