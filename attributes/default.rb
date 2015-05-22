b48827dfe9d37d1aa4bff54e5f98863e2e0af0a7
Brian Creasy <genetik@gmail.com>
2015-05-21 23:40:50 -0500

    new ip for ns1.caffeine.nu

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 node.default['local']['hostname'] = 'ns1.caffeine.nu'
-node.default['local']['ip4addr'] = '104.131.63.61'
+node.default['local']['ip4addr'] = '45.55.171.150'
 
 node.default['primary']['hostname'] = 'ns2.caffeine.nu'
 node.default['primary']['ip4addr'] = '208.52.168.244'
